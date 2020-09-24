1ea13e159de7ad85f68164b987d54fb25ee03b00
e-rojas <ejrojas@icloud.com>
2020-09-24 09:18:59 -0300

    separated the button component, also used the ScrolledView RNC

---
+++ components/ColorButton.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Text, View, TouchableHighlight, StyleSheet } from 'react-native';
+
+const ColorButton = ({
+    backgroundColor,onSelect=f=>f
+}) => (
+        <TouchableHighlight style={styles.button} onPress={() => onSelect(backgroundColor)} underlayColor='orange'>
+            <View style={styles.row} >
+                <View style={[styles.sample, { backgroundColor }]} />
+                <Text style={styles.text} > {backgroundColor}</Text>
+            </View>
+        </TouchableHighlight>
+    );
+
+const styles = StyleSheet.create({
+    button: {
+        // fontSize:30,
+        margin: 10,
+        padding: 10,
+        borderWidth: 2,
+        borderRadius: 10,
+        alignSelf: 'stretch',
+        backgroundColor: 'rgba(255,255,255,.8) '
+        // textAlign:'center'
+    }, row: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    }, sample: {
+        width: 20,
+        height: 20,
+        borderRadius: 10,
+        margin: 5,
+        backgroundColor: 'white'
+    }, text: {
+        fontSize: 30,
+        maregin: 5
+    }
+})
+export default ColorButton;
--- components/ColorList.js
+++ components/ColorList.js
@@ -9,7 +9,8 @@ import React from 'react';
    ImageBackground,
    TouchableHighlight
   } from 'react-native' */
-  import { View,Text,StyleSheet,TouchableHighlight } from 'react-native';
+  import { View,Text,StyleSheet,TouchableHighlight ,ScrollView} from 'react-native';
+  import ColorButton from './ColorButton';
 class ColorList extends React.Component {
     state = { 
         backgroundColor:'blue'
@@ -21,24 +22,30 @@ this.setState({backgroundColor})
         // Deconstruct the state 
         const { backgroundColor} = this.state
         return (
-            <View style={[styles.container,{backgroundColor}]} >
-                <TouchableHighlight style={styles.button} onPress={()=>this.changeColor('yellow')} underlayColor='orange'>
-                <View style={styles.row} >
-                    <View style={[styles.sample, {backgroundColor:'yellow'} ]} />
-                    <Text style={styles.text} > Yellow</Text>
-                </View>
-                </TouchableHighlight>
+            <ScrollView style={[styles.container,{backgroundColor}]} >
+                <ColorButton  backgroundColor='red' onSelect={(color)=> this.changeColor(color)}/>
+                <ColorButton  backgroundColor='blue' onSelect={(color)=> this.changeColor(color)}/>
+                <ColorButton  backgroundColor='green' onSelect={(color)=> this.changeColor(color)}/>
+                <ColorButton  backgroundColor='salmon' onSelect={(color)=> this.changeColor(color)}/>
+                <ColorButton  backgroundColor='grey' onSelect={(color)=> this.changeColor(color)}/>
+                <ColorButton  backgroundColor='white' onSelect={(color)=> this.changeColor(color)}/>
+                <ColorButton  backgroundColor='red' onSelect={(color)=> this.changeColor(color)}/>
+                <ColorButton  backgroundColor='blue' onSelect={(color)=> this.changeColor(color)}/>
+                <ColorButton  backgroundColor='green' onSelect={(color)=> this.changeColor(color)}/>
+                <ColorButton  backgroundColor='salmon' onSelect={(color)=> this.changeColor(color)}/>
+                <ColorButton  backgroundColor='grey' onSelect={(color)=> this.changeColor(color)}/>
+                <ColorButton  backgroundColor='white' onSelect={(color)=> this.changeColor(color)}/>
              {/*   <Text onPress={()=> this.changeColor('green')} style={styles.button} >Green</Text>
                <Text onPress={()=> this.changeColor('red')} style={styles.button} >Red</Text> */}
-            </View>
+            </ScrollView>
         );
     }
 }
 const styles = StyleSheet.create({
     container: {
         flex:1,
-        justifyContent:'center',
-        alignItems:'center'
+      /*   justifyContent:'center', no need for these on scroll view
+        alignItems:'center' */
     //   backgroundColor: '#DDD',
      /*  flexDirection:'column',
       height:'100vh',
@@ -46,28 +53,7 @@ const styles = StyleSheet.create({
       // justifyContent:'space-between'
       
     },
-    button:{
-        // fontSize:30,
-        margin:10,
-        padding:10,
-        borderWidth:2,
-        borderRadius:10,
-        alignSelf:'stretch',
-        backgroundColor:'rgba(255,255,255,.8) '
-        // textAlign:'center'
-    },row:{
-        flexDirection:'row',
-        alignItems:'center'
-    },sample:{
-        width:20,
-        height:20,
-        borderRadius:10,
-        margin:5,
-        backgroundColor:'white'
-    },text:{
-fontSize:30,
-maregin:5
-    }
+  
   })
 
 export default ColorList;
