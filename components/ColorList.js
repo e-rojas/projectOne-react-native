4ddd03c373ef682f94e376e0ddc3a09127250da3
e-rojas <ejrojas@icloud.com>
2020-09-24 08:37:24 -0300

    grouped elements with touchableHighlight comp, using underLayColor to show the touch button efx

--- components/ColorList.js
+++ components/ColorList.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import {
+/* import {
     StatusBar,
     Dimensions, // the dimensions object you can get the pixels value for the full screen
     View,
     Text,
     StyleSheet,
    Image,
-   ImageBackground
-  } from 'react-native'
+   ImageBackground,
+   TouchableHighlight
+  } from 'react-native' */
+  import { View,Text,StyleSheet,TouchableHighlight } from 'react-native';
 class ColorList extends React.Component {
     state = { 
         backgroundColor:'blue'
@@ -20,8 +22,14 @@ this.setState({backgroundColor})
         const { backgroundColor} = this.state
         return (
             <View style={[styles.container,{backgroundColor}]} >
-               <Text onPress={()=> this.changeColor('green')} style={styles.button} >Green</Text>
-               <Text onPress={()=> this.changeColor('red')} style={styles.button} >Red</Text>
+                <TouchableHighlight style={styles.button} onPress={()=>this.changeColor('yellow')} underlayColor='orange'>
+                <View style={styles.row} >
+                    <View style={[styles.sample, {backgroundColor:'yellow'} ]} />
+                    <Text style={styles.text} > Yellow</Text>
+                </View>
+                </TouchableHighlight>
+             {/*   <Text onPress={()=> this.changeColor('green')} style={styles.button} >Green</Text>
+               <Text onPress={()=> this.changeColor('red')} style={styles.button} >Red</Text> */}
             </View>
         );
     }
@@ -37,14 +45,28 @@ const styles = StyleSheet.create({
       padding:'5px' */
       // justifyContent:'space-between'
       
-    },button:{
-        fontSize:30,
+    },
+    button:{
+        // fontSize:30,
         margin:10,
         padding:10,
         borderWidth:2,
         borderRadius:10,
         alignSelf:'stretch',
-        textAlign:'center'
+        backgroundColor:'rgba(255,255,255,.8) '
+        // textAlign:'center'
+    },row:{
+        flexDirection:'row',
+        alignItems:'center'
+    },sample:{
+        width:20,
+        height:20,
+        borderRadius:10,
+        margin:5,
+        backgroundColor:'white'
+    },text:{
+fontSize:30,
+maregin:5
     }
   })
 
